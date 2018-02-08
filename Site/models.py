29a3b07d72200c305424077acc37203ff55a8626
dimariap <pdimaria@bu.edu>
2018-02-07 19:08:59 -0500

    added more columns to DB
    added city, state, date_posted, date_entered, is_sponsored, category, and skills to JobPosting
    
    rearranged order
    
    removed JobSkillAdmin (not necessary)

--- Site/models.py
+++ Site/models.py
@@ -5,29 +5,28 @@ from django.contrib import admin
 from django.template.defaultfilters import slugify
 from django.conf import settings
 import datetime
-# from django_mysql.models import ListCharField
+
+
+class JobSkill(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    skills = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
 class JobPosting(models.Model):
     title = models.CharField(max_length=64)
     company = models.CharField(blank=True, max_length=64)
     description = models.TextField(blank=True, max_length=10000)
-    # city
-    # state
-    # date posted
-    # date entered
-    # sponsored
-    # skills
+    city = models.CharField(blank=True, max_length=64)
+    state = models.CharField(blank=True, max_length=2)  # convert to abbreviation
+    date_posted = models.DateTimeField(null=True, blank=True, auto_now=False)
+    date_entered = models.DateTimeField(auto_now=True)
+    is_sponsored = models.BooleanField(verbose_name="Sponsored")
+    category = models.ForeignKey(JobSkill, null=True, blank=True, on_delete=models.CASCADE)
+    skills = models.CharField(blank=True, max_length=500)
 
 
 class JobPostingAdmin(admin.ModelAdmin):
     list_display = ('title', 'company')
-
-
-class JobSkill(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    # skills = ListCharField(blank=True, max_length=64)
-
-
-class JobSkillAdmin(admin.ModelAdmin):
-    list_display = ('name', )
