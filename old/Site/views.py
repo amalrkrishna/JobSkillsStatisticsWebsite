2b91d81cb779407389161909abdeac6f20ad546c
dimariap <pdimaria@bu.edu>
2018-02-19 15:43:49 -0500

    deleted 'old' directory

--- old/Site/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.shortcuts import render
-
-
-def home(request):
-    job_postings = JobPosting.objects.all()  # order by date
-
-    return render(request, 'home.html', {'job_postings': job_postings})
