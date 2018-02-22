5b9b8d57e251364f0d3ab0ffbc781a956b387f31
Amal R Krishna <amalrkrishna94@gmail.com>
2018-02-22 16:04:34 -0500

    Glassdoor page updated

--- Scraper/DisplayData.py
+++ Scraper/DisplayData.py
@@ -3,6 +3,7 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 from plotly.graph_objs import *
 from Site.models import *
+import os
 
 
 def GetSkillsFromJobRegion(job, region):
@@ -33,3 +34,63 @@ def GetSkillsFromJobRegion(job, region):
     plot_div = plot(figure, output_type='div', include_plotlyjs=False)
     '''return url'''
     return plot_div
+
+def GlassdoorPlot1():
+    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
+
+    USMetro = Jan2018[Jan2018['Measure'] == 'Metro Median Pay']
+    print(USMetro)
+
+    graphData = go.Bar(
+        x=USMetro['Metro'],
+        y=USMetro['Value'])
+    
+    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
+    graphLayout = go.Layout(
+        title='Median Pay in top US Cities',
+        showlegend=False,
+        autosize=False,
+        width=500,
+        height=400,
+        xaxis=dict(
+            autorange=True,
+        ),
+        yaxis=dict(
+            autorange=True,
+            title="Median Pay"
+        )
+        )
+
+    figure = go.Figure(data = [graphData], layout = graphLayout)
+    plot_USMetro = plot(figure, output_type='div', include_plotlyjs=False)
+    print(plot_USMetro)
+    return plot_USMetro
+
+def GlassdoorPlot2():
+    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
+
+    USJobOpen = Jan2018[Jan2018['Measure'] == 'Job Openings']
+
+    graphData = go.Bar(
+        x=USJobOpen['Metro'],
+        y=USJobOpen['Value'])
+    
+    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
+    graphLayout = go.Layout(
+        title='Job Openings in top US Cities',
+        showlegend=False,
+        autosize=False,
+        width=500,
+        height=400,
+        xaxis=dict(
+            autorange=True,
+        ),
+        yaxis=dict(
+            autorange=True,
+            title="Median Pay"
+        )
+        )
+
+    figure = go.Figure(data = [graphData], layout = graphLayout)
+    plot_USJobOpen = plot(figure, output_type='div', include_plotlyjs=False)
+    return plot_USJobOpen
