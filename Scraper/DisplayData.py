d49570536cfc714212c6b0a5a78af5b1adf7828d
Amal R Krishna <amalrkrishna94@gmail.com>
2018-03-01 02:51:50 -0500

    Glassdoor and Indeed pages updated. City and Job submission works at view and generates the graph. Added box plots for Glassdoor.

--- Scraper/DisplayData.py
+++ Scraper/DisplayData.py
@@ -29,180 +29,7 @@ def GetSkillsFromJobRegion(job, region):
     
     '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
     graphLayout = go.Layout(
-        showlegend=False,
-        autosize=False
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_div = plot(figure, output_type='div', include_plotlyjs=False)
-    '''return url'''
-    return plot_div
-
-def GlassdoorPlot1():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USMetro = Jan2018[Jan2018['Measure'] == 'Metro Median Pay']
-    print(USMetro)
-
-    graphData = go.Bar(
-        x=USMetro['Metro'],
-        y=USMetro['Value'])
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Median Pay in top US Cities',
-        showlegend=False,
-        autosize=False,
-        width=500,
-        height=400,
-        xaxis=dict(
-            autorange=True,
-        ),
-        yaxis=dict(
-            autorange=True,
-            title="Median Pay"
-        )
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_USMetro = plot(figure, output_type='div', include_plotlyjs=False)
-    print(plot_USMetro)
-    return plot_USMetro
-
-def GlassdoorPlot2():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USJobOpen = Jan2018[Jan2018['Measure'] == 'Job Openings']
-
-    graphData = go.Bar(
-        x=USJobOpen['Metro'],
-        y=USJobOpen['Value'],
-        marker=dict(color='black'),)
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Job Openings in top US Cities',
-        showlegend=False,
-        autosize=False,
-        width=500,
-        height=400,
-        xaxis=dict(
-            autorange=True,
-        ),
-        yaxis=dict(
-            autorange=True,
-            title="Job Openings"
-        )
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_USJobOpen = plot(figure, output_type='div', include_plotlyjs=False)
-    return plot_USJobOpen
-
-def GlassdoorPlot3():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USLabourForce = Jan2018[Jan2018['Measure'] == 'Labor Force Size']
-
-    graphData = go.Bar(
-        x=USLabourForce['Metro'],
-        y=USLabourForce['Value'],
-        marker=dict(color='red'),)
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Labor Force Size in top US Cities',
-        showlegend=False,
-        autosize=False,
-        width=500,
-        height=400,
-        xaxis=dict(
-            autorange=True,
-        ),
-        yaxis=dict(
-            autorange=True,
-            title="Labor"
-        )
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_USLabourForce = plot(figure, output_type='div', include_plotlyjs=False)
-    return plot_USLabourForce
-
-def GlassdoorPlot4():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USUnEmp = Jan2018[Jan2018['Measure'] == 'Unemployment Rate']
-
-    graphData = go.Bar(
-        x=USUnEmp['Metro'],
-        y=USUnEmp['Value'],
-        marker=dict(color='green'),)
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Unemployment Rate in top US Cities',
-        showlegend=False,
-        autosize=False,
-        width=500,
-        height=400,
-        xaxis=dict(
-            autorange=True,
-        ),
-        yaxis=dict(
-            autorange=True,
-            title="Unemployment Rate"
-        )
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_USUnEmp = plot(figure, output_type='div', include_plotlyjs=False)
-    return plot_USUnEmp
-
-def GlassdoorPlot5():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USTotEmp = Jan2018[Jan2018['Measure'] == 'Total Employment']
-
-    graphData = go.Bar(
-        x=USTotEmp['Metro'],
-        y=USTotEmp['Value'],
-        marker=dict(color='aqua'),)
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Total Employment in top US Cities',
-        showlegend=False,
-        autosize=False,
-        width=500,
-        height=400,
-        xaxis=dict(
-            autorange=True,
-        ),
-        yaxis=dict(
-            autorange=True,
-            title="Total Employment"
-        )
-        )
-
-    figure = go.Figure(data = [graphData], layout = graphLayout)
-    plot_USTotEmp = plot(figure, output_type='div', include_plotlyjs=False)
-    return plot_USTotEmp
-
-def GlassdoorPlot6():
-    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
-
-    USMedianPayBox = Jan2018[Jan2018['Measure'] == 'Median Base Pay']
-
-    graphData = go.Box(
-        x=USMedianPayBox['Metro'],
-        y=USMedianPayBox['Value'],
-        marker=dict(color='red'),
-        )
-    
-    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
-    graphLayout = go.Layout(
-        title='Median Pay box plot in top US Cities',
+        title='Skills vs Count',
         showlegend=False,
         autosize=False,
         width=1100,
@@ -212,7 +39,101 @@ def GlassdoorPlot6():
         ),
         yaxis=dict(
             autorange=True,
-            title="Median Pay"
+            title='Count'
+        )
+        )
+
+    figure = go.Figure(data = [graphData], layout = graphLayout)
+    plot_div = plot(figure, output_type='div', include_plotlyjs=False)
+    '''return url'''
+    return plot_div
+
+def GlassdoorPlot1(genstat):
+    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
+
+    USMetro = Jan2018[Jan2018['Measure'] == genstat]
+    print(USMetro)
+
+    graphData = go.Bar(
+        x=USMetro['Metro'],
+        y=USMetro['Value'])
+    
+    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
+    graphLayout = go.Layout(
+        title=genstat+' in top US Cities',
+        showlegend=False,
+        autosize=False,
+        width=1100,
+        height=800,
+        xaxis=dict(
+            autorange=True,
+        ),
+        yaxis=dict(
+            autorange=True,
+            title=genstat
+        )
+        )
+
+    figure = go.Figure(data = [graphData], layout = graphLayout)
+    plot_USMetro = plot(figure, output_type='div', include_plotlyjs=False)
+    print(plot_USMetro)
+    return plot_USMetro
+
+def GlassdoorPlot2(boxplot2):
+    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
+
+    USJobTitle = Jan2018[Jan2018['Dimension Type'] == boxplot2]
+
+    graphData = go.Box(
+        x=USJobTitle['Dimension'],
+        y=USJobTitle['Value'],
+        marker=dict(color='green'),
+        )
+    
+    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
+    graphLayout = go.Layout(
+        title=boxplot2+' box plot in top US Cities',
+        showlegend=False,
+        autosize=False,
+        width=1100,
+        height=800,
+        xaxis=dict(
+            autorange=True,
+        ),
+        yaxis=dict(
+            autorange=True,
+            title=boxplot2
+        )
+        )
+
+    figure = go.Figure(data = [graphData], layout = graphLayout)
+    plot_USMPBox = plot(figure, output_type='div', include_plotlyjs=False)
+    return plot_USMPBox
+
+def GlassdoorPlot6(boxplot):
+    Jan2018 = pd.read_excel('data/LPR_data-2018-01.xlsx')
+
+    USMedianPayBox = Jan2018[Jan2018['Measure'] == boxplot]
+
+    graphData = go.Box(
+        x=USMedianPayBox['Metro'],
+        y=USMedianPayBox['Value'],
+        marker=dict(color='red'),
+        )
+    
+    '''url = py.plot([graphData], output_type='div', include_plotlyjs=False)'''
+    graphLayout = go.Layout(
+        title=boxplot+' box plot in top US Cities',
+        showlegend=False,
+        autosize=False,
+        width=1100,
+        height=800,
+        xaxis=dict(
+            autorange=True,
+        ),
+        yaxis=dict(
+            autorange=True,
+            title=boxplot
         )
         )
 
--- Site/views.py
+++ Site/views.py
@@ -10,6 +10,8 @@ import pandas as pd
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from django.template.loader import render_to_string
+import plotly.plotly as py
+import plotly.graph_objs as go
 
 
 
@@ -29,8 +31,25 @@ def database(request):
 def landing_page(request):
     return render(request, 'landing.html')
 
+
+class IndeedPlot(TemplateView):
+    template_name = "indeed.html"
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(IndeedPlot, self).get_context_data(**kwargs)
+        context['plot'] = DisplayData.GetSkillsFromJobRegion(kwargs['job'], kwargs['city'])
+        return context
+
+@csrf_exempt
 def indeed(request):
-    return render(request, 'indeed.html')
+    if request.method == 'GET' and 'job' in request.GET:
+        job=request.GET['job']
+        city=request.GET['city']
+        g = IndeedPlot()
+        context = g.get_context_data(job=job, city=city)
+        return render(request, 'indeed.html', context)
+    else:
+        return render(request, 'indeed.html')
 
 class Plot(TemplateView):
     template_name = "plot.html"
@@ -39,28 +58,49 @@ class Plot(TemplateView):
         # Call the base implementation first to get a context
         context = super(Plot, self).get_context_data(**kwargs)
         context['plot'] = DisplayData.GetSkillsFromJobRegion("data analyst", "Boston, MA")
+        print(context['plot'])
         return context
 
 class PlotGlassDoor(TemplateView):
     template_name = "glassdoor.html"
-    
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(PlotGlassDoor, self).get_context_data(**kwargs)
-        context['plot1'] = DisplayData.GlassdoorPlot1()
-        context['plot2'] = DisplayData.GlassdoorPlot2()
-        context['plot3'] = DisplayData.GlassdoorPlot3()
-        context['plot4'] = DisplayData.GlassdoorPlot4()
-        context['plot5'] = DisplayData.GlassdoorPlot5()
-        context['plot6'] = DisplayData.GlassdoorPlot6()
+        context['plot'] = DisplayData.GlassdoorPlot1(kwargs['genstat'])
+        return context
+
+class PlotGlassDoorBox(TemplateView):
+    template_name = "glassdoor.html"
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PlotGlassDoorBox, self).get_context_data(**kwargs)
+        context['plot6'] = DisplayData.GlassdoorPlot6(kwargs['boxplot'])
+        return context
+
+class PlotGlassDoorBox2(TemplateView):
+    template_name = "glassdoor.html"
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(PlotGlassDoorBox2, self).get_context_data(**kwargs)
+        context['plot2'] = DisplayData.GlassdoorPlot2(kwargs['boxplot'])
         return context
 
 @csrf_exempt
-def indeed_form_submit(request):
-    if request.method == "POST":
-        city = request.POST.get('sel1')
-        job_title = request.POST.get('sel2')
-
-
-        indeed_plot=DisplayData.GetSkillsFromJobRegion(job_title, city)
-        return render(request, 'indeed.html', {'indeed_plot' : indeed_plot})
+def glassdoor(request):
+    if request.method == 'GET' and 'genstat' in request.GET:
+        genstat=request.GET['genstat']
+        g = PlotGlassDoor()
+        context = g.get_context_data(genstat=genstat)
+        return render(request, 'glassdoor.html', context)
+    elif request.method == 'GET' and 'boxplot' in request.GET:
+        boxplot=request.GET['boxplot']
+        if boxplot == 'Median Base Pay':
+            g = PlotGlassDoorBox()
+            context = g.get_context_data(boxplot=boxplot)
+            return render(request, 'glassdoor.html', context)
+        else:
+            g = PlotGlassDoorBox2()
+            context = g.get_context_data(boxplot=boxplot)
+            return render(request, 'glassdoor.html', context)
+    else:
+        return render(request, 'glassdoor.html')
